85295da54a0ef64a143150af65408d3a005e4c4b
DikiYLexa <dubovan36@gmail.com>
2025-12-16 15:23:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using TaskManagerTelegramBot_Дегтянников;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
