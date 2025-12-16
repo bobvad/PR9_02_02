f27c1cd5232a7a60adfd70463b479b91744bf86c
DikiYLexa <dubovan36@gmail.com>
2025-12-16 15:33:07 +0500

    Созданный классы для приложения. Дегтянников.

---
+++ Classes/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Дегтянников.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser) 
+        {
+            this.IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
